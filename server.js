bddd5d995b61134d52d495b62ecdbc29c356f86e
santhosh-2504 <santhoshanantha58@gmail.com>
2024-12-06 11:42:02 +0530

    Removed URL errors

--- server.js
+++ server.js
@@ -9,12 +9,9 @@ app.use(express.json());
 app.use(cors());
 
 // MongoDB connection
-mongoose.connect(
-    process.env.MONGO_URI,
-    { useNewUrlParser: true, useUnifiedTopology: true }
-)
-.then(() => console.log('Connected to MongoDB'))
-.catch(err => console.error('Failed to connect to MongoDB:', err));
+mongoose.connect(process.env.MONGO_URI, { serverSelectionTimeoutMS: 5000 })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Failed to connect to MongoDB:', err));
 
 // Schema and Model
 const dataSchema = new mongoose.Schema(
